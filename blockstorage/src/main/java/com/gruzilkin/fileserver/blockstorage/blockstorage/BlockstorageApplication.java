45c8a8fbb9f0d52e88395923c85f9cb0b7acddb9
gruzilkin <gruzmob@gmail.com>
2023-08-27 15:23:35 +0900

    add blockstorage module

---
+++ blockstorage/src/main/java/com/gruzilkin/fileserver/blockstorage/blockstorage/BlockstorageApplication.java
@@ -0,0 +1,13 @@
+package com.gruzilkin.fileserver.blockstorage.blockstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockstorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockstorageApplication.class, args);
+	}
+
+}
---
+++ blockstorage/src/test/java/com/gruzilkin/fileserver/blockstorage/blockstorage/BlockstorageApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gruzilkin.fileserver.blockstorage.blockstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.cassandra.CassandraAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@EnableAutoConfiguration(exclude = CassandraAutoConfiguration.class)
+class BlockstorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
